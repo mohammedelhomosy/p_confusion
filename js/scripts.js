52abd1c23dc79bd48ec379655ffc37a005ad648e
Mohammed ElHomosy <mohammed.elhomosy@gmail.com>
2021-07-26 12:15:26 +0200

    NPM Scripts Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
-    $("#mycarousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
+$(document).ready(function () {
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
         if ($("#carouselButton").children("span").hasClass('fa-pause')) {
             $("#mycarousel").carousel('pause');
             $("#carouselButton").children("span").removeClass('fa-pause');
             $("#carouselButton").children("span").addClass('fa-play');
         }
-        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        else if ($("#carouselButton").children("span").hasClass('fa-play')) {
             $("#mycarousel").carousel('cycle');
             $("#carouselButton").children("span").removeClass('fa-play');
-            $("#carouselButton").children("span").addClass('fa-pause');                    
+            $("#carouselButton").children("span").addClass('fa-pause');
         }
     });
-    $("#loginlink").click(function(){
+    $("#loginlink").click(function () {
         $("#loginModal").modal();
     });
-    $("#reservebtn").click(function(){
+    $("#reservebtn").click(function () {
         $("#ReserveModal").modal();
     });
 });
